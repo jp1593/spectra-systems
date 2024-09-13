996ba166c5aa23bc3940949a03836db98681b65d
Juan Pablo Estrada L <82290390+jp1593@users.noreply.github.com>
2024-09-12 18:58:00 -0600

    Update +page.svelte

--- apex-airline-system/frontend/src/routes/login/+page.svelte
+++ apex-airline-system/frontend/src/routes/login/+page.svelte
@@ -29,7 +29,7 @@
     >
       <div class="flex flex-col">
         <div class="flex items-center">
-          <div class="font-bold text-xl">Log In Ville Lumiere - Sept 12 2024 - Magic - UAT </div>
+          <div class="font-bold text-xl">Log In Ville Lumiere - Sept 12 2024 - Magic - UAT  </div>
           <Loader2
             class="shrink-0 ml-3 h-4 w-4 animate-spin {!loading && 'hidden'}"
           />
