dc03c0d90b2f5434f9318d5f0b08a341fb8eed77
Juan Pablo Estrada L <82290390+jp1593@users.noreply.github.com>
2024-10-10 18:18:29 -0600

    Update +page.svelte

--- apex-airline-system/frontend/src/routes/login/+page.svelte
+++ apex-airline-system/frontend/src/routes/login/+page.svelte
@@ -29,7 +29,7 @@
     >
       <div class="flex flex-col">
         <div class="flex items-center">
-          <div class="font-bold text-xl">Log In Ciudad de los reyes - Sept 12 2024 - Magic - Master  </div>
+          <div class="font-bold text-xl">Log In Ciudad de los reyes - Sept 12 2024 - Magic - Master - KENOBI  </div>
           <Loader2
             class="shrink-0 ml-3 h-4 w-4 animate-spin {!loading && 'hidden'}"
           />
